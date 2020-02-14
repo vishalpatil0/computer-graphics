b7dad1d67618305bd9cb5db17b5d8c7dc34654d5
Vishal Patil <46533610+vishalpatil0@users.noreply.github.com>
2020-02-14 15:41:54 +0530

    okk
    circle drawing algorithm

---
+++ bresenhams_circle.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<graphics.h>
+using namespace std;
+class vishal
+{
+        public:
+               void plot(int xc,int yc,int x,int y)
+               {        
+                        putpixel(xc+x,yc+y,1);// x,y y,x -x,y -y,x  -x,-y -y,-x x,-y y,-x
+                        putpixel(xc+y,yc+x,2);
+                        putpixel(xc-x,yc+y,3);
+                        putpixel(xc-y,yc+x,4);
+                        putpixel(xc-x,yc-y,5);
+                        putpixel(xc-y,yc-x,6);
+                        putpixel(xc+x,yc-y,7);
+                        putpixel(xc+y,yc-x,8);
+                }
+};                                       
+int main()
+{       
+        vishal c;
+        int gd=DETECT,gm,xc,yc,r,x,y,d;
+        initgraph(&gd,&gm,NULL);
+        cout<<"Enter the center co-ordinate"<<endl;
+        cin>>xc>>yc;
+        cout<<"Enter the readius"<<endl;
+        cin>>r;
+        x=0;
+        y=r;
+        c.plot(xc,yc,x,y);
+        d=3-2*r;
+        while(x<=y)
+        {
+                if(d<0)
+                {
+                        x=x+1;
+                        y=y;
+                        d=d+4*x+6;
+                }
+                else
+                {
+                        x=x+1;
+                        y=y-1;
+                        d=d+4*(x-y)+10;
+                }
+                 c.plot(xc,yc,x,y);
+        }
+        delay(10000);
+        closegraph(); 
+        return 0;
+}
